d18ef4b772e3adf43831bd366ca1d26266574f4a
Jefferson Vidal <vidalmoc@gmail.com>
2024-09-24 14:40:26 -0300

    Criados arquivos para Status do Sistema, Prioridades, Permissão de Acesso e Escritórios Extrenos

---
+++ app/Http/Controllers/ExternalOfficeController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ExternalOffice;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ExternalOfficeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ExternalOffice $externalOffice)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ExternalOffice $externalOffice)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ExternalOffice $externalOffice)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ExternalOffice $externalOffice)
+    {
+        //
+    }
+}
