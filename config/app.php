3465a68efe4669f626756310eb237c33419841ea
Jefferson Vidal <vidalmoc@gmail.com>
2024-11-22 12:07:53 -0300

    Correção de arquivos AppServiceProvider.php

--- config/app.php
+++ config/app.php
@@ -122,12 +122,5 @@ return [
         'driver' => env('APP_MAINTENANCE_DRIVER', 'file'),
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
-    'providers' => [
-        /*
-        * Application Service Providers...
-        */
-        App\Providers\AppServiceProvider::class,
-        // Outros providers...
-    ],
 
 ];
