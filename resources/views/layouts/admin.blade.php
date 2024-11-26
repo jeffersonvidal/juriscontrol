d3a4f80bf8175e596a4d8fc8b7f878ba2aa2ad37
Jefferson Vidal <vidalmoc@gmail.com>
2024-11-26 10:09:41 -0300

    Alteração no posicionamento da mensagem tastr de lembretes

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" integrity="sha512-3pIirOrwegjM6erE5gPSwkUzO+3cTjpnV9lexlNZqvupR64iZBnOOTiiLPb9M36zpMScbmUNIcHUqKD47M719g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <style>
         .toast-top-custom {
-            top: 50% !important; /* Ajuste este valor conforme necessário */
+            top: 20% !important; /* Ajuste este valor conforme necessário */
             right: 12px;
         }
     </style>
